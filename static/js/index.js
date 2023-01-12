2a5c86030037166f78b8a235e97e487b437dfae0
xxxace <731162160@qq.com>
2023-01-12 13:43:08 +0800

    更新代码

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,28 @@
 $(document).ready(function () {
     // 公共变量
     let isBgmPlay = false;
+    const app = $('#app');
+    const header = $('header');
 
     // 防止刷新后滚动条位置不变
-    setTimeout(() => {
-        $('.hidden').removeClass('hidden')
-    }, 10);
+    setTimeout(() => app.show(), 100);
 
     // 导航栏
     const headerMenuIndicator = $('.header-menu-indicator')
-    $('#headerMenu').on('mousemove', function (e) {
+    $('#headerMenu').mousemove(function (e) {
         if (e.target.nodeName === 'A') {
             headerMenuIndicator.attr('style', `transform:translateX(${e.target.offsetLeft + 30}px)`)
         } else {
             headerMenuIndicator.attr('style', `transform:translateX(${30}px)`)
+        }
+    });
+
+    // 滚动监听
+    $(window).on('scroll', function () {
+        if ($(document).scrollTop() > 60) {
+            header.addClass('header-scroll')
+        } else {
+            header.removeClass('header-scroll')
         }
     });
 
@@ -34,8 +43,7 @@ $(document).ready(function () {
         bgmBtn.toggleClass('off')
     }
 
-    bgmBtn.on('click', function () {
-
+    bgmBtn.click(function () {
         if (bgmMusic.paused) {
             bgmPlay()
         } else {
@@ -49,7 +57,7 @@ $(document).ready(function () {
     video_wrapper.on('click', function (e) {
         if (e.target.tagName === 'BUTTON') {
             video2[0].pause()
-            video_wrapper.attr('style', 'display:none')
+            video_wrapper.hide()
             video_wrapper.parent().attr('style', '')
 
             if (isBgmPlay) {
@@ -61,9 +69,9 @@ $(document).ready(function () {
 
     // 视频播放
     const video_btn = $('#video_btn')
-    video_btn.on('click', function () {
+    video_btn.click(function () {
         video_wrapper.parent().attr('style', 'overflow:hidden')
-        video_wrapper.attr('style', '')
+        video_wrapper.show()
         video2[0].play()
 
         if (!bgmMusic.paused) {
@@ -73,9 +81,18 @@ $(document).ready(function () {
     });
 
     // 轮播图
-    const newsCarousel = new bootstrap.Carousel('#newsCarousel', {
+    new bootstrap.Carousel('#newsCarousel', {
         interval: 2500,
         ride: 'carousel',
         touch: false
     });
+
+    // 新闻
+    const newsTabs = $('.news-tabs')
+    newsTabs.click(function (e) {
+        if (e.target.tagName === 'LI') {
+            $(this.querySelectorAll('.active')).removeClass("active")
+            $(e.target).addClass("active")
+        }
+    })
 })
